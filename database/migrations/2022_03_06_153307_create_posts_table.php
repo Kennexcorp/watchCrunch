68b869a29b427a6d935e8bb711035fcaf8ad6805
Kene Sylvester O <ekeneoguikpu@yahoo.com>
2022-03-07 22:06:00 +0100

    Update 2022_03_06_153307_create_posts_table.php

--- database/migrations/2022_03_06_153307_create_posts_table.php
+++ database/migrations/2022_03_06_153307_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title', 255);
             $table->foreignIdFor(\App\Models\User::class);
             $table->timestamps();
         });
